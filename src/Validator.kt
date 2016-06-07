24dc35b7764b3ea5fc73fa1c07068b56a3adb4aa
Andrey Shcherbakov <veontomo@gmail.com>
2016-06-07 11:57:22 +0200

    feat: set up the project

---
+++ src/Validator.kt
@@ -0,0 +1,8 @@
+/**
+ * Created by Andrey on 07/06/2016.
+ */
+class Validator {
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+}
---
+++ src/app.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrey on 07/06/2016.
+ */
+fun main(args: Array<String>) {
+    val c = Validator();
+    print(c.sum(2, -1));
+
+}
+
