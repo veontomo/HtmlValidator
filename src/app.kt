af5226fa4c9a9a825b316ce6e81328e43dc099dc
Andrey Shcherbakov <veontomo@gmail.com>
2016-06-07 15:31:09 +0200

    feat: add Jsoup library

--- src/app.kt
+++ src/app.kt
@@ -1,9 +1,14 @@
 /**
  * Created by Andrey on 07/06/2016.
  */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
 fun main(args: Array<String>) {
     val c = Validator();
     print(c.sum(2, -1));
-
+    val doc = Jsoup.connect("http://en.wikipedia.org/").get();
+    val newsHeadlines = doc.select("#mp-itn b a");
 }
 
