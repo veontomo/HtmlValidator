86ff572bf90bf9329a3ceefae0c5de750083f503
Andrey Shcherbakov <veontomo@gmail.com>
2016-06-07 16:51:49 +0200

    refactor: create an ascii validator
    Move the code from the main function into a separate class that is named
    as AsciiValidator.

--- src/AsciiValidator.kt
+++ src/AsciiValidator.kt
@@ -2,4 +2,17 @@
  * Created by Andrey on 07/06/2016.
  */
 class AsciiValidator {
+    public val content: String;
+
+    constructor(text: String) {
+        content = text;
+    }
+
+    fun isValid(): Boolean {
+        return content.toCharArray().map { x -> x.toInt() }.all {
+            x ->
+            x >= 32 && x <= 126
+        }
+
+    }
 }
