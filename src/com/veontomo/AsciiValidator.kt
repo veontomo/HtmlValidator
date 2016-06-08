73ddcf018eedff999a3cdebeff7acacd116f5e8d
Andrey Shcherbakov <veontomo@gmail.com>
2016-06-08 16:28:43 +0200

    chore: create a package structure
    Create nested folders in "src" in order order to represent the required project
    structure and put the source code inside those folders.

---
+++ src/com/veontomo/AsciiValidator.kt
@@ -0,0 +1,15 @@
+package com.veontomo
+
+/**
+ * Created by Andrey on 07/06/2016.
+ */
+class AsciiValidator {
+
+    fun isValid(text: String): Boolean {
+        return text.toCharArray().map { x -> x.toInt() }.all {
+            x ->
+            x >= 32 && x <= 126
+        }
+
+    }
+}
