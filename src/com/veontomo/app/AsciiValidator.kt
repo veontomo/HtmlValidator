fb87615943b6f58093906304f9f02e3c666e12bc
Andrey Shcherbakov <veontomo@gmail.com>
2016-06-09 10:00:38 +0200

    feat: print out the lines with non-ascii characters

--- src/com/veontomo/app/AsciiValidator.kt
+++ src/com/veontomo/app/AsciiValidator.kt
@@ -1,5 +1,8 @@
 package com.veontomo.app
 
+import java.io.File
+import java.util.*
+
 /**
  * Created by Andrey on 07/06/2016.
  */
@@ -10,6 +13,22 @@ class AsciiValidator {
             x ->
             x >= 32 && x <= 126
         }
+    }
+
+
+    fun analyzeFile(fileName: String) {
+        val invalid : HashMap<Int, String> = hashMapOf();
+        File(fileName).readLines().forEachIndexed { i, s ->
+            if (!isValid(s)) {
+                invalid.put(i+1, s);
+            }
+        }
+        if (invalid.size == 0){
+            print("File " + fileName + " does not contain non-ascii characters.")
+        } else {
+            invalid.forEach { i, s -> println("Line no. " + i + ": "+ s + " contains non-ascii characters."); }
+        }
 
     }
+
 }
