2bbcd1ef0012f280e7c040601837a7fa8793d7df
Andrey Shcherbakov <veontomo@gmail.com>
2016-09-14 08:29:20 +0200

    feat: visualize non-ascii symbols

--- src/com/veontomo/app/AsciiValidator.kt
+++ src/com/veontomo/app/AsciiValidator.kt
@@ -4,32 +4,21 @@ import java.io.File
 import java.util.*
 
 /**
- * Created by Andrey on 07/06/2016.
+ * Check presence of non-ascii characters.
  */
 class AsciiValidator {
-
-    fun isValid(text: String): Boolean {
-        return text.toCharArray().map { x -> x.toInt() }.all {
-            x ->
-            x >= 32 && x <= 126
-        }
+    fun filterOutValid(text: String): List<Char> {
+        return text.toCharArray().filter { c -> (c.toInt() < 32 && c.toInt() != 9) || c.toInt() > 126 }
     }
 
-
     fun analyzeFile(file: File) {
-        val invalid : HashMap<Int, String> = hashMapOf();
+        println("File ${file.name} anomalies: ")
         file.readLines().forEachIndexed { i, s ->
-            if (!isValid(s)) {
-                invalid.put(i+1, s);
+            val res = filterOutValid(s)
+            if (!res.isEmpty()) {
+                print("line $i contains non-ascii symbols: ${res.joinToString { it -> "$it (ascii code: ${it.toInt()})" }}")
             }
         }
-        if (invalid.size == 0){
-            print("File " + file.name + " does not contain non-ascii characters.")
-        } else {
-            println("File " + file.name)
-            invalid.forEach { i, s -> println("Line no. " + i + ": "+ s + " contains non-ascii characters."); }
-        }
-
     }
 
 }
