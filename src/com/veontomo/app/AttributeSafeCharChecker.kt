2bfd6ace537f500ee7b75452d00c2ded7c538fc0
Andrey Shcherbakov <veontomo@gmail.com>
2016-11-16 09:32:20 +0100

    fix: add _ to the set of safe chars for attr values

--- src/com/veontomo/app/AttributeSafeCharChecker.kt
+++ src/com/veontomo/app/AttributeSafeCharChecker.kt
@@ -7,12 +7,12 @@ import org.jsoup.nodes.Element
  * Find a non-safe characters inside attribute values of html tags.
  *
  * Only these characters are considered safe for an html tag attribute value:
- * 0-9, a-z, A-Z, ;, :, ., comma, space, &, #, !, ?, -, +, =, /, $, %, (, )
+ * 0-9, a-z, A-Z, ;, :, ., comma, space, &, #, !, ?, -, +, =, /, $, %, (, ), _
  *
  */
 class AttributeSafeCharChecker : Checker() {
 
-    private val safeChars = " 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz;:.,&#!?-+=/$%()".toSet()
+    private val safeChars = " 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz;:.,&#!?-+=/$%()_".toSet()
     /**
      * Check the validity of given string and return a list of messages related to found irregularities.
      * @param html string whose validity is to be checked. Assume it is a valid html document.
