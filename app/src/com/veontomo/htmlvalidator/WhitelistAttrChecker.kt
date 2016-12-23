e248ad3bd1629138cf5c988e06b12bc09f58563d
Andrey Shcherbakov <veontomo@gmail.com>
2016-12-23 12:16:37 +0100

    feat: stage the tests for the white list attr checker

--- app/src/com/veontomo/htmlvalidator/WhitelistAttrChecker.kt
+++ app/src/com/veontomo/htmlvalidator/WhitelistAttrChecker.kt
@@ -1,14 +1,23 @@
 package com.veontomo.htmlvalidator
 
+import org.jsoup.nodes.Element
+
 /**
  * Detect presence of attributes that are not in "white list".
  *
- * Every element is allowed to have attributes only from a list, called "white list".
+ * There are two types of attributes:
+ * 1. attributes of the form name="value", i.e.: attribute "href" in this tag:
+ * <a href="www.google.com"  ...>...</a>
+ * 2. attributes of the "style" one:
+ * <a style="color: blue;text-decoration: underline;">...</a>
  *
+ * Every element is allowed to have attributes of the first type from the set "attrPlain" and
+ * the attributes of the second type from the set "attrInline".
  *
  */
 class WhitelistAttrChecker : Checker() {
-    private val whitelist = listOf("title", "style", "href", "alt",
+    private val attrPlain = setOf("title", "href", "width", "height", "alt", "src")
+    private val attrInline = setOf(
             "width", "max-width", "min-width",
             "padding", "padding-top", "padding-bottom", "padding-left", "padding-right",
             "margin", "margin-top", "margin-bottom", "margin-left", "margin-right",
@@ -16,9 +25,37 @@ class WhitelistAttrChecker : Checker() {
             "font-size", "font-weight", "font-family",
             "border", "border-style", "border-spacing",
             "color"
-            )
+    )
 
     override fun check(html: String): List<CheckMessage> {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    }
+
+    /**
+     * Check whether the element contains only allowed attributes.
+     *
+     * @param el element whose attributes are to be inspected.
+     * @param attrs set of allowed attributes
+     * @return true if all element's attribute are in the set "attrs", false otherwise.
+     */
+    fun checkAttr(el: Element, attrs: Set<String>): Boolean {
+        return el.attributes().all { it -> attrs.contains(it.key) }
+    }
+
+    /**
+     * Check whether the "style" attribute of the element contains only the keys from the
+     * given set.
+     * @param el element whose style attribute is to be inspected
+     * @param attrs set of allowed keys in the style attribute
+     * @return true if
+     */
+    fun checkInline(el: Element, attrs: Set<String>) : Boolean {
+        return el.attr("style")
+                .split(";")
+                .map { it -> it.split(":") }
+                .filter { it.size == 2 }
+                .map { it[0].trim() }
+                .filterNot { attrs.contains(it) }
+                .isEmpty()
     }
 }
---
+++ app/src/test/com/veontomo/htmlvalidator/WhitelistAttrCheckerTest.kt
@@ -0,0 +1,21 @@
+package test.com.veontomo.htmlvalidator
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Andrey on 23/12/2016.
+ */
+class WhitelistAttrCheckerTest {
+    @Test
+    fun checkAttr() {
+
+    }
+
+    @Test
+    fun checkInline() {
+
+    }
+
+}
