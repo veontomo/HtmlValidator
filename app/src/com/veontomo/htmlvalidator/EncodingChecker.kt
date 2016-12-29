57a3327f13abb18445c2411fd12b33ab1b1ae004
Andrey Shcherbakov <veontomo@gmail.com>
2016-12-29 16:46:23 +0100

    feat: extract the charset

--- app/src/com/veontomo/htmlvalidator/EncodingChecker.kt
+++ app/src/com/veontomo/htmlvalidator/EncodingChecker.kt
@@ -4,17 +4,40 @@ import org.jsoup.Jsoup
 
 /**
  * Check the encoding.
- * An html document encoding should be set in tag "meta" as follows:
- *
+ * An html document might have the charset impoosed in the tag "meta" in one of the two ways:
+
+ * <meta charset="ascii">
  * <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
  * @param encoding list of allowed encodings
  */
-class EncodingChecker(val encodings: List<String>): Checker() {
+class EncodingChecker(val encodings: List<String>) : Checker() {
     override fun check(html: String): List<CheckMessage> {
-        val doc = Jsoup.parse(html)
-        val metas = doc.select("meta")
+        return listOf()
     }
 
-    override val descr  = "Encoding checker"
+    override val descr = "Encoding checker"
 
+    /**
+     * Return all the charsets as it is specified in the meta tag inside the attribute "content" or "charset".
+     * If no charset is found, empty set is returned.
+     * @param html string representation of the html document
+     * @return list of all found charsets. Might be empty.
+     */
+    fun getCharset(html: String): Set<String> {
+        val token1 = "charset"
+        val token2 = "content"
+        val result = mutableSetOf<String>()
+        val doc = Jsoup.parse(html)
+        val metas = doc.select("meta")
+        for (meta in metas){
+            if (meta.hasAttr(token1)){
+                result.add(meta.attr(token1))
+            }
+            if (meta.hasAttr(token2)){
+                result.addAll(meta.attr(token2).split(";").filter { it.matches(Regex("\\s?charset=.*$")) }.map { it.replace("charset=", "").trim() })
+            }
+        }
+        return result
+   }
 }
--- app/src/test/com/veontomo/htmlvalidator/EncodingCheckerTest.kt
+++ app/src/test/com/veontomo/htmlvalidator/EncodingCheckerTest.kt
@@ -1,5 +1,6 @@
 package test.com.veontomo.htmlvalidator
 
+import com.veontomo.htmlvalidator.EncodingChecker
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -8,9 +9,101 @@ import org.junit.Assert.*
  *  Test suite for checking the encoding.
  */
 class EncodingCheckerTest {
-    @Test
-    fun check() {
+    /**
+     * Test for the method that picks up the charset.
+     * Partition strategy:
+     * 1. # meta tags: 0, 1, > 1
+     * 2. location of the charset: none, in "charset", in "content", in both
+     */
 
+    // Cover
+    // 1. # meta tags: 0
+    // 2. location of charset: none
+    @Test
+    fun checkZeroMetaTags() {
+        val html =
+                "<!DOCTYPE html><html><head><title>Title</title></head><body></body><html>"
+        assertTrue(EncodingChecker(listOf()).getCharset(html).isEmpty())
+    }
+
+    // Cover
+    // 1. # meta tags: 1
+    // 2. location of charset: none
+    @Test
+    fun checkOneMetaTagsNoCharset() {
+        val html =
+                "<!DOCTYPE html><html><head><meta http-equiv=\"content-type\" content=\"text/html;\">" +
+                        "<title>Title</title></head><body></body><html>"
+//        assertNull(EncodingChecker(listOf()).getCharset(html))
+        assertTrue(EncodingChecker(listOf()).getCharset(html).isEmpty())
+    }
+
+    // Cover
+    // 1. # meta tags: > 1
+    // 2. location of charset: none
+    @Test
+    fun checkTwoMetaTags() {
+        val html =
+                "<!DOCTYPE html><html><head><meta http-equiv=\"content-type\" content=\"text/html;\">" +
+                        "<meta name=\"description\" content=\"another meta\">" +
+                        "<title>Title</title></head><body></body><html>"
+        assertTrue(EncodingChecker(listOf()).getCharset(html).isEmpty())
+    }
+
+    // Cover
+    // 1. # meta tags: 1
+    // 2. location of charset: in "charset"
+    @Test
+    fun checkOneMetaTagsCharset() {
+        val html =
+                "<!DOCTYPE html><html><head><meta charset=\"ascii\">" +
+                        "<title>Title</title></head><body></body><html>"
+        val charsets = EncodingChecker(listOf()).getCharset(html)
+        assertEquals(1, charsets.size)
+        assertTrue(charsets.contains("ascii"))
+    }
+
+    // Cover
+    // 1. # meta tags: 1
+    // 2. location of charset: in "content"
+    @Test
+    fun checkOneMetaTagsContent() {
+        val html =
+                "<!DOCTYPE html><html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">" +
+                        "<title>Title</title></head><body></body><html>"
+        val charsets = EncodingChecker(listOf()).getCharset(html)
+        assertEquals(1, charsets.size)
+        assertTrue(charsets.contains("utf-8"))
+    }
+
+    // Cover
+    // 1. # meta tags: > 1
+    // 2. location of charset: in both
+    @Test
+    fun checkTwoMetaTagsInBoth() {
+        val html =
+                "<!DOCTYPE html><html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">" +
+                        "<meta charset=\"ascii\">" +
+                        "<title>Title</title></head><body></body><html>"
+//        assertNull(EncodingChecker(listOf()).getCharset(html))
+//        assertTrue(EncodingChecker(listOf()).getCharset(html).isEmpty())
+        val charsets = EncodingChecker(listOf()).getCharset(html)
+        assertEquals(2, charsets.size)
+        assertTrue(charsets.containsAll(listOf("utf-8", "ascii")))
+    }
+
+    // Cover
+    // 1. # meta tags: > 1
+    // 2. location of charset: in content
+    @Test
+    fun checkTwoMetaTagsInContent() {
+        val html =
+                "<!DOCTYPE html><html><head><meta http-equiv=\"content-type\" content=\"text/html;charset=ascii\">" +
+                        "<meta name=\"description\" content=\"another meta\">" +
+                        "<title>Title</title></head><body></body><html>"
+        val charsets = EncodingChecker(listOf()).getCharset(html)
+        assertEquals(1, charsets.size)
+        assertTrue(charsets.contains("ascii"))
     }
 
 }
