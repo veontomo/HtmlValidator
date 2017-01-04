dc3c54cae3481e72c309ef0e08cc600bb2f15ea1
Andrey Shcherbakov <veontomo@gmail.com>
2017-01-04 10:19:14 +0100

    feat: add the JavaFX into the project

--- src/main/java/com/veontomo/htmlvalidator/main.kt
+++ src/main/java/com/veontomo/htmlvalidator/main.kt
@@ -2,11 +2,26 @@ package com.veontomo.htmlvalidator
 
 import java.io.File
 
+import javafx.application.Application
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.scene.layout.GridPane
+import javafx.scene.layout.HBox
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
+import javafx.scene.text.FontWeight
+import javafx.scene.text.Text
+import javafx.stage.Stage
 
 /**
  * A script that makes the checkers run against given files.
  */
 fun main(args: Array<String>) {
+    GUI.main(args)
     val attrPlain = setOf("title", "href", "width", "height", "alt", "src", "style", "target", "http-equiv", "content", "cellpadding", "cellspacing")
     val attrInline = setOf(
             "width", "max-width", "min-width",
@@ -53,4 +68,49 @@ fun runCheckers(file: File, checkers: List<Checker>): Map<String, List<CheckMess
     return checkers.associateBy({ it.descriptor }, { it.check(text) })
 }
 
+class GUI : Application() {
+    override fun start(primaryStage: Stage) {
+        primaryStage.setTitle("JavaFX Welcome")
+        val grid = GridPane()
+        grid.setAlignment(Pos.CENTER)
+        grid.setHgap(10.0)
+        grid.setVgap(10.0)
+        grid.setPadding(Insets(25.0, 25.0, 25.0, 25.0))
+        val scenetitle = Text("Welcome")
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20.0))
+        grid.add(scenetitle, 0, 0, 2, 1)
+
+        val actiontarget = Text("hidden")
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20.0))
+        grid.add(actiontarget, 0, 4, 2, 1)
+
+        val userName = Label("User Name:")
+        grid.add(userName, 0, 1)
+
+        val userTextField = TextField()
+        grid.add(userTextField, 1, 1)
+
+        val btn = Button("Sign in")
+        val hbBtn = HBox(10.0)
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT)
+        hbBtn.getChildren().add(btn)
+        grid.add(hbBtn, 1, 4)
+        btn.setOnAction {e ->
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Sign in button pressed");
+
+        }
+        val scene = Scene(grid, 300.0, 275.0)
+        primaryStage.setScene(scene)
+        primaryStage.show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch(GUI::class.java)
+        }
+    }
+
+}
 
