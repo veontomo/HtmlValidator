df300f934556743d0f604e150397945be60e51ef
Andrey Shcherbakov <veontomo@gmail.com>
2017-01-13 10:28:52 +0100

    feat: re-activate the user controls

--- src/main/kotlin/com/veontomo/htmlvalidator/Controller/AnalyzerController.kt
+++ src/main/kotlin/com/veontomo/htmlvalidator/Controller/AnalyzerController.kt
@@ -3,6 +3,7 @@ package com.veontomo.htmlvalidator.Controller
 import com.veontomo.htmlvalidator.*
 import com.veontomo.htmlvalidator.Models.*
 import com.veontomo.htmlvalidator.Views.MainView
+import javafx.collections.FXCollections
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.MenuItem
@@ -12,9 +13,11 @@ import javafx.scene.input.KeyCodeCombination
 import javafx.scene.input.KeyCombination
 import javafx.scene.text.Text
 import javafx.scene.web.WebView
+import javafx.stage.FileChooser
 import javafx.stage.Stage
 import java.io.File
 import java.net.URL
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -29,12 +32,18 @@ class AnalyzerController : Initializable {
     @FXML private var menuAnalyze: MenuItem? = null
     @FXML private var menuClear: MenuItem? = null
 
+    /**
+     * Name of the file that stores the preferences
+     */
+    val pref = "HtmlValidator.history"
+
     // keyboard shortcut for selecting a file "Ctrl+o"
     val fileSelectShortcut = KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN)
     // keyboard shortcut for analyzing a selected file "Ctrl+Enter"
     val analyzeShortcut = KeyCodeCombination(KeyCode.ENTER, KeyCombination.CONTROL_DOWN)
     // keyboard shortcut for clearing the results "Ctrl+C"
     val clearShortcut = KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN)
+
     val attrPlain = setOf("title", "href", "width", "height", "alt", "src", "style", "target",
             "http-equiv", "content", "cellpadding", "cellspacing", "lang", "border")
     val attrInline = setOf(
@@ -54,20 +63,17 @@ class AnalyzerController : Initializable {
 
     var selectedFile: File? = null
 
-    init {
-//        menuSelect.setOnAction { fileChooserController?.onSelect() }
-//        menuAnalyze.setOnAction { analyzerController?.onAnalyze() }
-//        menuClear.setOnAction { analyzerController?.onClear() }
-//        view.enableSelect(true)
-//        view.enableAnalyze(false)
-//        view.enableClear(false)
-//        view.loadItems(createEmptyReport())
-    }
-
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         menuSelect!!.accelerator = fileSelectShortcut
         menuAnalyze!!.accelerator = analyzeShortcut
         menuClear!!.accelerator = clearShortcut
+        menuSelect!!.setOnAction { onSelect() }
+        menuAnalyze!!.setOnAction { onAnalyze() }
+        menuClear!!.setOnAction { onClear() }
+        enableSelect(true)
+        enableAnalyze(false)
+        enableClear(false)
+        loadItems(createEmptyReport())
     }
 
     private fun performCheck(file: File): List<Report> {
@@ -96,12 +102,12 @@ class AnalyzerController : Initializable {
     fun onAnalyze() {
         val file = selectedFile
         if (file != null) {
-//            view.enableAnalyze(false)
-//            view.enableSelect(false)
-//            val reports = performCheck(file)
-//            view.loadItems(reports)
-//            view.enableAnalyze(true)
-//            view.enableSelect(true)
+            enableAnalyze(false)
+            enableSelect(false)
+            val reports = performCheck(file)
+            loadItems(reports)
+            enableAnalyze(true)
+            enableSelect(true)
         }
     }
 
@@ -128,5 +134,126 @@ class AnalyzerController : Initializable {
     private fun createEmptyReport(): List<Report> {
         return checkers.map { Report(it.descriptor, null, null) }
     }
+
+    /**
+     * Display the content of the selected file inside the web viewer.
+     * @param url
+     */
+    fun showFileContent(url: String?) {
+        browser!!.engine.load(url)
+    }
+
+    /**
+     * Enable/disable a menu item that is used to analyze the file
+     * @param isEnabled true to enable, false to disable
+     */
+    fun enableAnalyze(isEnabled: Boolean) {
+        menuAnalyze!!.isDisable = !isEnabled
+    }
+
+    /**
+     * Enable/disable a menu item that is used to select a file
+     * @param isEnabled true to enable, false to disable
+     */
+    fun enableSelect(isEnabled: Boolean) {
+        menuSelect!!.isDisable = !isEnabled
+    }
+
+    /**
+     * Enable/disable a menu item that is used to clear the result of analysis
+     * @param isEnabled true to enable, false to disable
+     */
+    fun enableClear(isEnabled: Boolean) {
+        menuClear!!.isDisable = !isEnabled
+    }
+
+    /**
+     * Load the items in to the list view
+     * @param items
+     */
+    fun loadItems(items: List<Report>) {
+        checkersView!!.items = FXCollections.observableArrayList(items)
+    }
+
+
+    fun onSelect() {
+        val allowedExtensions = listOf("html", "htm")
+        val last = readLastUsedFileName()
+        val fileChooser = FileChooser()
+        if (last != null) {
+            val file = File(last)
+            if (file.exists() && file.isDirectory)
+                fileChooser.initialDirectory = file
+        }
+        fileChooser.extensionFilters.addAll(
+                FileChooser.ExtensionFilter("html", allowedExtensions.map { "*.$it" })
+        )
+        fileChooser.title = "Select a file"
+        val file = fileChooser.showOpenDialog(checkersView!!.scene.window)
+
+        if (file?.exists() ?: false) {
+            if (allowedExtensions.contains(file.extension)) {
+                showFileName(file!!.path)
+                showFileContent(file.toURI().toURL().toExternalForm())
+                enableAnalyze(true)
+                onFileSelected(file)
+                enableClear(true)
+                showFileInfo(file)
+                saveLastUsedDir(file.parent)
+            }
+        } else {
+            enableAnalyze(false)
+        }
+    }
+
+
+    /**
+     * Gets info about file (its last modification time) and displays it.
+     * @param file
+     */
+    private fun showFileInfo(file: File) {
+        val time = Date(file.lastModified())
+        val formatter = SimpleDateFormat("dd MMMM yyyy HH:mm:ss", Locale("Italy"))
+        val data = formatter.format(time)
+        fileInfoText!!.text = if (!data.isBlank()) "Last modified: $data" else null
+    }
+
+    /**
+     * A folder from which a file that has been chosen last time.
+     * @return name of a folder or null.
+     */
+    private fun readLastUsedFileName(): String? {
+        val prefFile = File(pref)
+        if (prefFile.exists()) {
+            return prefFile.readText()
+        }
+        return null
+    }
+
+    /**
+     * Save folder name where the file is located.
+     * @param dirName folder name
+     */
+    private fun saveLastUsedDir(dirName: String) {
+        val file = File(pref)
+        file.writeText(dirName)
+    }
+
+    /**
+     * Inform the appropriate controller that the user has selected a file.
+     * @param file
+     */
+    fun onFileSelected(file: File) {
+        selectedFile = file
+    }
+
+    /**
+     * Display the selected file name
+     * @param name file name
+     */
+    fun showFileName(name: String) {
+        fileNameText!!.text = name
+    }
+
 }
 
--- src/main/kotlin/com/veontomo/htmlvalidator/Views/MainView.kt
+++ src/main/kotlin/com/veontomo/htmlvalidator/Views/MainView.kt
@@ -1,7 +1,6 @@
 package com.veontomo.htmlvalidator.Views
 
 import com.veontomo.htmlvalidator.Controller.AnalyzerController
-import com.veontomo.htmlvalidator.Controller.FileChooserController
 import com.veontomo.htmlvalidator.Models.Report
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
@@ -19,7 +18,7 @@ class MainView(val stage: Stage) {
     val checkerStatusCol = TableColumn<Report, String>("Status")
     val checkerCommentCol = TableColumn<Report, String>("Comment")
     var analyzerController = AnalyzerController()
-    var fileChooserController: FileChooserController? = null
+//    var fileChooserController: FileChooserController? = null
 
     fun getScene(): Scene {
         val grid = FXMLLoader.load<GridPane>(javaClass.getResource("/MainView.fxml"))
@@ -27,67 +26,6 @@ class MainView(val stage: Stage) {
         return scene
     }
 
-    /**
-     * Display the selected file name
-     * @param name file name
-     */
-    fun showFileName(name: String) {
-//        fileNameText.text = name
-    }
-
-    /**
-     * Display file info
-     * @param data information to display
-     */
-    fun showFileInfo(data: String) {
-//        fileInfoText.text = if (!data.isBlank()) "Last modified: $data" else null
-    }
-
-    /**
-     * Display the content of the selected file inside the web viewer.
-     * @param url
-     */
-    fun showFileContent(url: String?) {
-//        browser.engine.load(url)
-    }
-
-    /**
-     * Enable/disable a menu item that is used to analyze the file
-     * @param isEnabled true to enable, false to disable
-     */
-    fun enableAnalyze(isEnabled: Boolean) {
-//        menuAnalyze.isDisable = !isEnabled
-    }
-
-    /**
-     * Enable/disable a menu item that is used to select a file
-     * @param isEnabled true to enable, false to disable
-     */
-    fun enableSelect(isEnabled: Boolean) {
-//        menuSelect.isDisable = !isEnabled
-    }
-
-    /**
-     * Enable/disable a menu item that is used to clear the result of analysis
-     * @param isEnabled true to enable, false to disable
-     */
-    fun enableClear(isEnabled: Boolean) {
-//        menuClear.isDisable = !isEnabled
-    }
-
-    /**
-     * Load the items in to the list view
-     * @param items
-     */
-    fun loadItems(items: List<Report>) {
-//        checkersView.items = FXCollections.observableArrayList(items)
-    }
-
-    /**
-     * Inform the appropriate controller that the user has selected a file.
-     * @param file
-     */
-    fun onFileSelected(file: File) {
-//        analyzerController?.setFile(file)
-    }
 }
+
+
