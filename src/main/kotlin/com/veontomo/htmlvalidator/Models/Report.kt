bf8f7048cfd3b4e81cd883ed7b965c342cb5d058
Andrey Shcherbakov <veontomo@gmail.com>
2017-01-13 14:48:04 +0100

    feat: associate the Report class with the table view

--- src/main/kotlin/com/veontomo/htmlvalidator/Models/Report.kt
+++ src/main/kotlin/com/veontomo/htmlvalidator/Models/Report.kt
@@ -1,19 +1,37 @@
 package com.veontomo.htmlvalidator.Models
 
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 
 /**
  * Report
  */
-data class Report(val name: String, val status: Boolean?, val comment: String?)
-//    private val nameProp = SimpleStringProperty("")
-//    private val statusProp = SimpleBooleanProperty()
-//    private val commentProp = SimpleStringProperty(null)
+class Report() {
 
-//    : this()  {
-//        nameProp.set(name)
-//        if (status != null) statusProp.set(status)
-//        if (comment != null) commentProp.set(comment)
-//
-//    }
+    private val nameProp = SimpleStringProperty(null)
+    private val statusProp = SimpleObjectProperty<Boolean?>(true)
+    private val commentProp = SimpleStringProperty(null)
+
+    constructor(name: String, status: Boolean?, comment: String?) : this() {
+        nameProp.set(name)
+        if (status != null) statusProp.set(status)
+        if (comment != null) commentProp.set(comment)
+    }
+
+    var name
+        set(value) {
+            nameProp.set(value)
+        }
+        get() = nameProp.get()
+    var status
+        set(value) {
+            statusProp.set(value)
+        }
+        get() = statusProp.get()
+    var comment
+        set(value) {
+            commentProp.set(value)
+        }
+        get() = commentProp.get()
+}
