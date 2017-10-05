8fbb6ca78eef6456437149e5a728b618f6304c06
Andrey Shcherbakov <veontomo@gmail.com>
2017-10-05 07:44:24 +0200

    Make the method visitHtmlElement() throw a checked exception

--- src/main/kotlin/com/veontomo/htmlvalidator/parser/HtmlNodeParser.kt
+++ src/main/kotlin/com/veontomo/htmlvalidator/parser/HtmlNodeParser.kt
@@ -4,9 +4,9 @@ import com.veontomo.htmlvalidator.html.HTMLParser
 import com.veontomo.htmlvalidator.html.HTMLParserBaseVisitor
 import java.util.*
 
-class HtmlNodeParser : HTMLParserBaseVisitor<HtmlNode?>() {
+class HtmlNodeParser : HTMLParserBaseVisitor<HtmlNode>() {
 
-    override fun visitHtmlElement(ctx: HTMLParser.HtmlElementContext?): HtmlNode? {
+    override fun visitHtmlElement(ctx: HTMLParser.HtmlElementContext?): HtmlNode {
         println("visiting ${ctx?.text}")
         val tags = ctx?.htmlTagName()?.map { it.TAG_NAME().text }
         println(tags)
@@ -15,8 +15,7 @@ class HtmlNodeParser : HTMLParserBaseVisitor<HtmlNode?>() {
         check(size < 3, { "Tag list must contain 0, 1 or 2 elements, instead it contains $size elements." })
         if (size == 2) {
             if (tags[0] != tags[1]) {
-                return null
-//                "Opening and closing tags must be equal, ${ctx.text} instead ${tags[0]} and ${tags[1]} are given."
+                throw Exception("Opening and closing tags must be equal, instead ${tags[0]} and ${tags[1]} are given.")
             }
 
         }
@@ -34,9 +33,11 @@ class HtmlNodeParser : HTMLParserBaseVisitor<HtmlNode?>() {
         }
         val elems = ctx.htmlContent()?.htmlElement()
         elems?.forEach {
-            val n = visitHtmlElement(it)
-            n?.let {
-                node.appendChild(it)
+            try {
+                val n = visitHtmlElement(it)
+                node.appendChild(n)
+            } catch (e: Exception) {
+                println("Error while visiting element${it.text}: ${e.message}")
             }
         }
         return node
